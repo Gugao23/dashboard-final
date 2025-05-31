25e69d3ea518f420c5564cda44dde8838e0afd87
Gustavo Baeta <152343913+Gugao23@users.noreply.github.com>
2025-05-31 15:22:34 -0300

    autoP

--- src/app/pages/estoque/addform/addform.ts
+++ src/app/pages/estoque/addform/addform.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-addform',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './addform.html',
-  styleUrl: './addform.scss'
+  styleUrls: ['./addform.scss']
 })
 export class AddformComponent {
+  produto = '';
+  categoria = '';
+  tamanho = '';
+  quantidade: number | null = null;
+  preco: number | null = null;
 
+  produtosInfo: any = {
+    'CamisetaBasic': { categoria: 'Camiseta', preco: 119.99 },
+    'CalçaVermelha': { categoria: 'Calça', preco: 139.99 },
+    'CamisetaSwag': { categoria: 'Camiseta', preco: 119.99 },
+    'MoletomOff': { categoria: 'Moletom', preco: 129.99 },
+    'MoletomVermelho': { categoria: 'Moletom', preco: 149.99 },
+    'MoletomPurpleJack': { categoria: 'Moletom', preco: 139.99 }
+  };
+
+  onProdutoChange() {
+    const info = this.produtosInfo[this.produto];
+    if (info) {
+      this.categoria = info.categoria;
+      this.preco = info.preco;
+    } else {
+      this.categoria = '';
+      this.preco = null;
+    }
+  }
 }
