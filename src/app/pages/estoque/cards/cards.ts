67f6fb8c0b02a5cb229bf606a5d7ddcc5e73abf1
Gustavo Baeta <152343913+Gugao23@users.noreply.github.com>
2025-06-01 12:55:08 -0300

    estoqueatt

--- src/app/pages/estoque/cards/cards.ts
+++ src/app/pages/estoque/cards/cards.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './cards.html',
-  styleUrl: './cards.scss'
+  styleUrls: ['./cards.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  totalProdutos: number = 0;
+  estoqueStatus: 'verde' | 'amarelo' | 'vermelho' = 'verde';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.atualizarTotal();
+  }
+
+  atualizarTotal() {
+    this.http.get<any[]>('http://localhost:3000/produtosEstoque').subscribe(produtos => {
+      this.totalProdutos = produtos.reduce((soma, item) => soma + (item.quantidade || 0), 0);
+
+      // Define a cor do card de acordo com o total
+      if (this.totalProdutos > 200) {
+        this.estoqueStatus = 'verde';
+      } else if (this.totalProdutos >= 100) {
+        this.estoqueStatus = 'amarelo';
+      } else {
+        this.estoqueStatus = 'vermelho';
+      }
+    });
+  }
 }
